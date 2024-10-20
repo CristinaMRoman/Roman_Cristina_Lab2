c3f8c02e8680ada16d7d67524d02600028d421de
Cristina Roman <cristinamariaroman31@gmail.com>
2024-10-20 21:41:40 +0300

    Add project files.

---
+++ Data/Roman_Cristina_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Roman_Cristina_Lab2.Models;
+
+namespace Roman_Cristina_Lab2.Data
+{
+    public class Roman_Cristina_Lab2Context : DbContext
+    {
+        public Roman_Cristina_Lab2Context (DbContextOptions<Roman_Cristina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Roman_Cristina_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Roman_Cristina_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
