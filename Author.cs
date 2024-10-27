6235d80115e84b6d686b05e5750a78236e455559
CristinaMRoman <cristinamariaroman31@gmail.com>
2024-10-27 20:28:13 +0200

    Add files via upload

---
+++ Author.cs
@@ -0,0 +1,10 @@
+﻿namespace Roman_Cristina_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List <Book>? Books { get; set; } //navigation property
+    }
+}
